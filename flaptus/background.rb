a701b641fdc497894a82643e8e470e719dad5dbb
Patrick Winters <19wintersp@gmail.com>
2021-10-30 14:20:22 +0100

    Update background class

--- flaptus/background.rb
+++ flaptus/background.rb
@@ -1,7 +1,20 @@
-module Background
-	IMAGE = Gosu::Image.new("#{ROOT_PATH}/assets/images/backgroundp.png", tileable: false)
+class Background
+	IMAGE = Gosu::Image.new("#{ROOT_PATH}/assets/images/backgroundp.png")
 
-	def self.draw(x, y, z)
-		IMAGE.draw(x, y, z)
+	def initialize
+		@image = Background::IMAGE
+		@x = 0.0
+	end
+
+	def move(speed)
+		@x -= speed * 0.1
+		if @x < -@image.width
+			@x = 0
+		end
+	end
+
+	def draw
+		@image.draw(@x, 0, ZOrder::BACKGROUND)
+		@image.draw(@x + @image.width, 0, ZOrder::BACKGROUND)
 	end
 end
