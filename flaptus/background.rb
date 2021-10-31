65a33989b69c51bcd37385555bb8512f69a75e8c
Patrick Winters <19wintersp@gmail.com>
2021-10-31 13:41:39 +0000

    Update background rendering coe

--- flaptus/background.rb
+++ flaptus/background.rb
@@ -1,20 +1,17 @@
 class Background
-	IMAGE = Gosu::Image.new("#{ROOT_PATH}/assets/images/backgroundp.png")
-
 	def initialize
-		@image = Background::IMAGE
-		@x = 0.0
+		@image = Gosu::Image.new("#{ROOT_PATH}/assets/images/backgroundp.png")
+		@x = 0
 	end
 
 	def move(speed)
 		@x -= speed * 0.1
-		if @x < -@image.width
+		if @x < WIDTH - @image.width
 			@x = 0
 		end
 	end
 
 	def draw
-		@image.draw(@x + @image.width - 1, 0, ZOrder::BACKGROUND)
 		@image.draw(@x, 0, ZOrder::BACKGROUND)
 	end
 end
--- flaptus/foreground.rb
+++ flaptus/foreground.rb
@@ -1,18 +1,17 @@
 class Foreground
 	def initialize
 		@image = Gosu::Image.new("#{ROOT_PATH}/assets/images/foreground.png")
-		@x = 0.0
+		@x = 0
 	end
 
 	def move(speed)
 		@x -= speed * 0.5
-		if @x < -@image.width
+		if @x < WIDTH - @image.width
 			@x = 0
 		end
 	end
 
 	def draw
-		@image.draw(@x + @image.width - 1, 0, ZOrder::FOREGROUND)
 		@image.draw(@x, 0, ZOrder::FOREGROUND)
 	end
 end
